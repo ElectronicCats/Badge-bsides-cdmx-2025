6a444bec7fe7d834779dc72f92b891a60af85d18
deimos <deimoshall@gmail.com>
2025-06-18 21:04:56 -0600

    feat: add ssd1306 support

--- firmware/User/py32f0xx_it.c
+++ firmware/User/py32f0xx_it.c
@@ -1,41 +1,6 @@
-/**
-  ******************************************************************************
-  * @file    py32f0xx_it.c
-  * @author  MCU Application Team
-  * @brief   Interrupt Service Routines.
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) Puya Semiconductor Co.
-  * All rights reserved.</center></h2>
-  *
-  * <h2><center>&copy; Copyright (c) 2016 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
-  *
-  ******************************************************************************
-  */
-
-/* Includes ------------------------------------------------------------------*/
-#include "py32f0xx_hal.h"
+#include "main.h"
 #include "py32f0xx_it.h"
 
-/* Private includes ----------------------------------------------------------*/
-/* Private typedef -----------------------------------------------------------*/
-/* Private define ------------------------------------------------------------*/
-/* Private macro -------------------------------------------------------------*/
-/* Private variables ---------------------------------------------------------*/
-/* Private function prototypes -----------------------------------------------*/
-/* Private user code ---------------------------------------------------------*/
-/* External variables --------------------------------------------------------*/
-
-/******************************************************************************/
-/*          Cortex-M0+ Processor Interruption and Exception Handlers          */
-/******************************************************************************/
 /**
   * @brief This function handles Non maskable interrupt.
   */
@@ -72,14 +37,4 @@ void PendSV_Handler(void)
   */
 void SysTick_Handler(void)
 {
-  HAL_IncTick();
 }
-
-/******************************************************************************/
-/* PY32F0xx Peripheral Interrupt Handlers                                     */
-/* Add here the Interrupt Handlers for the used peripherals.                  */
-/* For the available peripheral interrupt handler names,                      */
-/* please refer to the startup file.                                          */
-/******************************************************************************/
-
-/************************ (C) COPYRIGHT Puya *****END OF FILE******************/
